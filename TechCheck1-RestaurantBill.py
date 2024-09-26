bf6991fbca328555c8395a6664aa4ebb31d8f9c1
Zeddyare <w0433704@nscc.ca>
2024-09-26 11:18:56 -0300

    Final Product Program

--- TechCheck1-RestaurantBill.py
+++ TechCheck1-RestaurantBill.py
@@ -10,6 +10,31 @@
 
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
+    
+    #Input asking for bill total
+    billed=input("Enter your bill amount: $")
+
+    #math
+    billed=float(billed) 
+    #made bill variable a float in case billed total doesn't land on a whole number 
+    tax=billed*1.15-billed 
+    tip=billed*1.2-billed 
+    total=billed+tax+tip
+    #also as billed was a float already theres no need to include the float conversions anymore
+
+    #output for totals 
+    #print("\nYour original billed amount was: ${0:,.2f}".format(billed)) 
+    #print("Your tax due is: ${0:,.2f}".format(tax))
+    #print("Your tip amount is: ${0:,.2f}".format(tip))
+    #print("Your total due is: ${0:,.2f}".format(total)) 
+    #gonna try to clean this up
+
+    print("""\nYour original billed amount was: ${0:,.2f}
+Your tax due is: ${1:,.2f}
+Your tip amount is: ${2:,.2f}
+Your total due is: ${3:,.2f}
+    """.format(billed, tax, tip, total))
+    print("Thank you for dining with us!") 
 
 
 
